aa21bce1a95ded04c456501fe9c79fe777080927
Jason Yu <me@ycmjason.com>
2017-07-21 22:21:39 +0800

    added polyfill and changed facebook/twitter link

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -2,30 +2,33 @@
   <footer class="pb-4 pt-4 mt-4">
     <div class="container">
       <p>
-        <a href="//facebook.com" target="_blank">
+        <a :href="facebook_link" target="_blank">
           <layout-icon name="facebook-official" size="md"></layout-icon>
         </a>
-        <a href="//twitter.com" target="_blank" class="ml-2">
+        <a :href="twitter_link" target="_blank" class="ml-2">
           <layout-icon name="twitter" size="md"></layout-icon>
         </a>
       </p>
 
       <p>
-        &copy; 2017 ClearBreathe | All Rights Reserved
+        &copy; 2017 AirBreathe Limited | All Rights Reserved
       </p>
     </div>
   </footer>
 </template>
 
 <script>
-export default { }
+export default {
+  data(){
+    return {
+      facebook_link: 'https://facebook.com/airbreathemask',
+      twitter_link: 'https://twitter.com/AirBreatheMask',
+    }
+  }
+}
 </script>
 
 <style scoped>
-#logo{
-  max-width: 100%;
-  width: 40rem;
-}
 footer{
   background-color: #eee;  
 }
--- client/src/components/SubscriptionForm.vue
+++ client/src/components/SubscriptionForm.vue
@@ -5,15 +5,15 @@
       <layout-input v-model="email" class="mr-1" type="email" placeholder="name@example.com" required></layout-input> 
       <layout-input type="submit">Subscribe</layout-input> 
     </layout-form>
-    <layout-alert type="success" v-if="successful">You are added to the mailing list!</layout-alert>
-    <layout-alert type="danger" v-if="failed">Opps! Something went wrong... Please try again!</layout-alert>
+    <layout-alert type="success" class="mt-3" v-if="successful">You are added to the mailing list!</layout-alert>
+    <layout-alert type="danger" class="mt-3" v-if="failed">Opps! Something went wrong... Please try again!</layout-alert>
   </div>
 </template>
 
 <script>
 import subscriptionsService from '../services/subscriptions.service';
 export default { 
-  data: () => {
+  data(){
     return {
       successful: false,
       failed: false,
